7772b349ac382285b8e31c1be2acfac68e864f31
Kangping-git <kangping@outlook.jp>
2025-06-23 17:03:09 +0900

    画像APIを見やすくした

--- backend/web/src/routers/api.ts
+++ backend/web/src/routers/api.ts
@@ -12,17 +12,7 @@ sharp.cache({
 
 router.get('/', (req, res) => {});
 router.get('/users', async (req, res) => {
-    const {
-        q = '',
-        country = void 0,
-        minAlgo,
-        maxAlgo,
-        minHeuristic,
-        maxHeuristic,
-        sort = 'algoRating',
-        limit = 50,
-        cursor,
-    } = req.query;
+    const { q = '', country = void 0, minAlgo, maxAlgo, minHeuristic, maxHeuristic, sort = 'algoRating', limit = 50, cursor } = req.query;
 });
 router.get('/users/:name/', (req, res) => {});
 
@@ -31,36 +21,23 @@ router.get('/users/:name/submissions/:submissionId', (req, res) => {});
 
 router.get('/users/:name/history', (req, res) => {});
 
-router.get('/users/:name/rating.svg', async (req, res) => {
+router.get(['/users/:name/rating.svg', '/users/:name/rating.png'], async (req, res) => {
     const { name } = req.params;
     const { isHeuristic, withPerformance, withSubmissions } = req.query;
+    const svg = await getUserRatingImage(name, isHeuristic === 'true', withPerformance === 'true', withSubmissions === 'true');
+    if (req.path.endsWith('.png')) {
+        res.setHeader('Content-Type', 'image/png');
+        const pngBuffer = await sharp(Buffer.from(svg)).png().toBuffer();
+        res.send(pngBuffer);
+        return;
+    }
     res.setHeader('Content-Type', 'image/svg+xml');
-    res.send(
-        await getUserRatingImage(name, isHeuristic === 'true', withPerformance === 'true', withSubmissions === 'true'),
-    );
-});
-router.get('/users/:name/rating.png', async (req, res) => {
-    const { name } = req.params;
-    const { isHeuristic, withPerformance, withSubmissions } = req.query;
-    res.setHeader('Content-Type', 'image/png');
-    const svgCode = await getUserRatingImage(
-        name,
-        isHeuristic === 'true',
-        withPerformance === 'true',
-        withSubmissions === 'true',
-    );
-    const pngBuffer = await sharp(Buffer.from(svgCode)).png().toBuffer();
-    res.send(pngBuffer);
+    res.send(svg);
 });
 router.get(['/users/:name/ratingGraph.svg', '/users/:name/ratingGraph.png'], async (req, res) => {
     const { name } = req.params;
     const { isHeuristic, withPerformance, withSubmissions } = req.query;
-    const svg = await getHistoryImage(
-        name,
-        isHeuristic === 'true',
-        withPerformance === 'true',
-        withSubmissions === 'true',
-    );
+    const svg = await getHistoryImage(name, isHeuristic === 'true', withPerformance === 'true', withSubmissions === 'true');
     if (req.path.endsWith('.png')) {
         res.setHeader('Content-Type', 'image/png');
         const pngBuffer = await sharp(Buffer.from(svg)).png().toBuffer();
